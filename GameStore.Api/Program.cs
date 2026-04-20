d9da0faf2e23669f18f8c471a139a23276a5ed9d
khalid El Marouani <khalid.elmarouani@gmail.com>
2026-04-20 17:20:24 +0200

    feature:Configuring logging levels and Data seeding

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,5 +1,6 @@
 using GameStore.EndPoints;
 using GameStore.Data;
+using GameStore.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,10 +8,29 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddValidation();
 
 var connString = "Data source=GameStore.db";
-builder.Services.AddSqlite<GameStoreContext>(connString);
+builder.Services.AddSqlite<GameStoreContext>(connString,
+optionsAction: options => options.UseSeeding((context, _) =>
+{
+    if (!context.Set<Genre>().Any())
+    {
+        context.Set<Genre>().AddRange(
+            new Genre { Name = "Action" },
+            new Genre { Name = "Adventure" },
+            new Genre { Name = "RPG" },
+            new Genre { Name = "Strategy" },
+            new Genre { Name = "Sports" },
+            new Genre { Name = "Racing" },
+            new Genre { Name = "Fighting" }
+        );
+        context.SaveChanges();
+    }
+})
+);
 
 var app = builder.Build();
 
 app.MapGamesEndPoints();
+
+app.MigrateDb();
 
 app.Run();
