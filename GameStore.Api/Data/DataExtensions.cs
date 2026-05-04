c0f7df851869b5966292b6ba3e6dfdf7d55f82fb
khalid El Marouani <khalid.elmarouani@gmail.com>
2026-05-04 16:35:13 +0200

    feature:Data seeding

--- GameStore.Api/Data/DataExtensions.cs
+++ GameStore.Api/Data/DataExtensions.cs
@@ -1,3 +1,4 @@
+using GameStore.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Data;
@@ -10,4 +11,26 @@ public static class DataExtensions
     db.Database.Migrate();
 
 }
+public static void AddGameStoreDb(this WebApplicationBuilder builder)
+    {
+        var connString = "Data source=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString,
+optionsAction: options => options.UseSeeding((context, _) =>
+{
+    if (!context.Set<Genre>().Any())
+    {
+        context.Set<Genre>().AddRange(
+            new Genre { Name = "Action" },
+            new Genre { Name = "Adventure" },
+            new Genre { Name = "RPG" },
+            new Genre { Name = "Strategy" },
+            new Genre { Name = "Sports" },
+            new Genre { Name = "Racing" },
+            new Genre { Name = "Fighting" }
+        );
+        context.SaveChanges();
+    }
+})
+);
+    }
 }
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -6,26 +6,7 @@ using GameStore.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
-
-var connString = "Data source=GameStore.db";
-builder.Services.AddSqlite<GameStoreContext>(connString,
-optionsAction: options => options.UseSeeding((context, _) =>
-{
-    if (!context.Set<Genre>().Any())
-    {
-        context.Set<Genre>().AddRange(
-            new Genre { Name = "Action" },
-            new Genre { Name = "Adventure" },
-            new Genre { Name = "RPG" },
-            new Genre { Name = "Strategy" },
-            new Genre { Name = "Sports" },
-            new Genre { Name = "Racing" },
-            new Genre { Name = "Fighting" }
-        );
-        context.SaveChanges();
-    }
-})
-);
+builder.AddGameStoreDb();
 
 var app = builder.Build();
 
